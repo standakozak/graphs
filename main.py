cdc9a88c0cc154bdaf65ff6407c7d479dc1fddb6
standakozak <standa23k@seznam.cz>
2020-05-13 21:33:27 +0200

    Created Vertex and Graph objects.

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-"# graphs" 
+class Graph:
+    """
+    Representation of graphs
+    """
+
+    def __init__(self, components=None):
+        """
+        Create graph object from set of vertex names
+        """
+
+        self.vertices = []
+        self.edges = []
+
+        for component in components:
+            vertex = Vertex(component)
+            self.vertices.append(vertex)
+
+
+class Vertex:
+    def __init__(self, name):
+        self.name = name
+        self.neighbors = []
