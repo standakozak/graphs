a1dfbe8b7b17315aececf232f08bae4cc84b598c
standakozak <standa23k@seznam.cz>
2020-05-26 21:07:22 +0200

    Adding edges on canvas, moving with vertices

--- app.py
+++ app.py
@@ -3,34 +3,57 @@ from tkinter import messagebox as messagebox
 from sys import exit
 
 from graph_class import Graph
-from draw_functions import add_vertex_from_click, draw_graph, check_position_on_canvas, remove_object, update_canvas
-from constants import VERTEX_SIZE, SCALE
+from draw_functions import *
 
 
 def click(event):
     event.widget.focus_set()
     if type(event.widget) == tk.Canvas:
-        global focused_object_index
-        global element_indexes
+        global focused_object_index, element_indexes
 
-        focused_object_index = check_position_on_canvas(event.x,event.y,graph_canvas,element_indexes,graph,tk.CURRENT)
+        focused_object_index = check_pos_set_focus(graph_canvas, element_indexes, graph, tk.CURRENT)
         if focused_object_index is None:
             focused_object_index, new_vertex = add_vertex_from_click(event.x, event.y, event.widget, graph)
             element_indexes[focused_object_index] = new_vertex
         element_indexes = update_canvas(graph_canvas, element_indexes, graph)
 
 
+def mouse_motion(event, mouse_num=1):
+    global dragged_object_index, element_indexes, graph, focused_object_index
+
+    if str(event.type) == "Motion":
+        if mouse_num == 1:
+            if dragged_object_index is None:
+                dragged_object_index = check_pos(graph_canvas, element_indexes, tk.CURRENT)
+                if dragged_object_index is None:
+                    dragged_object_index = False
+        elif mouse_num == 3:
+            if not focused_object_index:
+                focused_object_index = check_pos_set_focus(graph_canvas, element_indexes, graph, tk.CURRENT)
+            if focused_object_index:
+                move_focused(event.x, event.y, focused_object_index, element_indexes)
+                update_canvas(graph_canvas, element_indexes, graph)
+
+    elif str(event.type) == "ButtonRelease":
+        if event.num == 1:
+            if dragged_object_index:
+                end_object_index = check_pos(graph_canvas, element_indexes, tk.CURRENT)
+                if end_object_index is not None and end_object_index != dragged_object_index:
+                    new_edge_index, edge = add_line((element_indexes[dragged_object_index],
+                                                     element_indexes[end_object_index]), event.widget, graph)
+                    element_indexes[new_edge_index] = edge
+                    focused_object_index = new_edge_index
+            element_indexes = update_canvas(graph_canvas, element_indexes, graph)
+            dragged_object_index = None
+
+
 def key_press(event):
     if event.keysym == "Delete":
-        global graph
-        global element_indexes
-        global focused_object_index
+        global graph, element_indexes, focused_object_index
 
         graph = remove_object(focused_object_index, element_indexes, graph, graph_canvas)
         element_indexes = update_canvas(graph_canvas, element_indexes, graph)
         focused_object_index = None
-    #print(str(event.keysym))
-    #print(event.keycode)
 
 
 def setup():
@@ -43,7 +66,11 @@ def setup():
     global graph_canvas
     graph_canvas = tk.Canvas(frame, bg="#ffffff", bd=1.5, relief="sunken")
     graph_canvas.bind("<Button-1>", click)
+    graph_canvas.bind("<Button-3>", click)
     graph_canvas.bind("<Key>", key_press)
+    graph_canvas.bind("<B1-Motion>", mouse_motion)
+    graph_canvas.bind("<B3-Motion>", lambda event: mouse_motion(event, 3))
+    graph_canvas.bind("<ButtonRelease-1>", mouse_motion)
     graph_canvas.place(relheight=0.75, relwidth=0.8, relx=0.1, rely=0.075)
 
     _root.protocol("WM_DELETE_WINDOW", lambda: exit_window(_root))
@@ -59,11 +86,13 @@ def exit_window(tkroot):
 def loop(tkroot):
     global element_indexes
     element_indexes = draw_graph(graph, graph_canvas, element_indexes)
-    tk.mainloop()
+    tkroot.mainloop()
 
 
 if __name__ == "__main__":
     focused_object_index = None
+    dragged_object_index = None
+
     element_indexes = dict()
     graph = Graph()
     _test_dict = {
