d176b304f554346a3b653a6ad515e6e0fe1cd133
standakozak <standa23k@seznam.cz>
2020-05-27 13:32:56 +0200

    Added comments

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-VERTEX_SIZE = 30  # Width and height of ovals - vertices
+VERTEX_SIZE = 30  # Width and height of ovals (vertices)
 
 # For calculating position of randomly placed vertices. Change if vertices are created outside canvas
 MAX_CANVAS_WIDTH = 1024
