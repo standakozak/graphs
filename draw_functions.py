f509c68947b9f086a927f8e19279169e987ac3b4
standakozak <standa23k@seznam.cz>
2020-05-27 13:32:33 +0200

    Added docstrings

--- draw_functions.py
+++ draw_functions.py
@@ -3,6 +3,15 @@ from constants import VERTEX_SIZE
 
 
 def check_pos(canvas, elements, CURRENT):
+    """
+    Checks elements on CURRENT position
+
+    Returns ID of element on CURRENT position or None
+    Parameters:
+        canvas - tkinter.Canvas object
+        elements - dictionary of elements on canvas
+        CURRENT - tkinter.CURRENT
+    """
     return_value = None
     if CURRENT is not None:
         index = canvas.find_withtag(CURRENT)
@@ -14,10 +23,10 @@ def check_pos(canvas, elements, CURRENT):
 
 def check_pos_set_focus(canvas, elements, graph, CURRENT):
     """
-    Sets / unsets focus on CURRENT element from elements
+    Create / delete focus of CURRENT element from elements
 
-     Returns None if CURRENT is None, False if component got unfocused and index of element from dictionary
-     if it got focus.
+     Returns None if CURRENT is None or if there is not any element on CURRENT position,
+        False if component got unfocused and index of element from dictionary if it got focus.
 
     Parameters:
         canvas - tkinter.Canvas object
@@ -43,11 +52,12 @@ def update_canvas(canvas, elements, graph):
     """
     Draws elements of canvas, removes redundant elements from canvas
 
+    Objects from elements have to be in graph object to be drawn, otherwise they are deleted
+    Returns: modified dictionary of elements
     Parameters:
         canvas - tkinter.Canvas object
         elements - dictionary of elements on canvas
         graph - Graph object
-    Returns: modified dictionary of elements
     """
     indexes_to_delete = []
     for index, component in elements.items():
@@ -69,48 +79,57 @@ def update_canvas(canvas, elements, graph):
     return elements
 
 
-def draw_graph(_graph, canvas, elements):
+def draw_graph(graph, canvas, elements):
     """
     Initial drawing of graph
 
-    Adds graph edges and vertices to dictionary
+    Adds edges and vertices from graph to dictionary
     Returns: modified dictionary
+    Parameters:
+        graph - instance of Graph class
+        canvas - tkinter.Canvas object
+        elements - dictionary of elements on canvas
     """
-    for edge in _graph.edges:
+    for edge in graph.edges:
         element_index = draw_edge(canvas, edge)
         elements[element_index] = edge
-    for vertex in _graph.vertices:
+    for vertex in graph.vertices:
         element_index = draw_vertex(canvas, vertex)
         elements[element_index] = vertex
 
     return elements
 
 
-def draw_vertex(_canvas, _vertex, index=None):
+def draw_vertex(canvas, vertex, index=None):
     """
     Draws vertex on its position on canvas.
 
     Draws vertex or modify drawn vertex by its index on canvas
     Returns: element index if it was created.
+    Parameters:
+        canvas - tkinter.Canvas object
+        vertex - instance of Vertex class
+        index - int, ID of element on canvas (if already created)
+
     """
-    if _vertex.focused:
-        _color = "#befc9a"
+    if vertex.focused:
+        color = "#befc9a"
     else:
-        _color = "#ffffff"
+        color = "#ffffff"
     if index is None:
-        vertex_oval = _canvas.create_oval(_vertex.x - VERTEX_SIZE / 2, _vertex.y - VERTEX_SIZE / 2,
-                                          _vertex.x + VERTEX_SIZE / 2, _vertex.y + VERTEX_SIZE / 2,
-                                          width=2, fill=_color, activeoutline="green", activewidth=3
-                                          )
-        _canvas.tag_raise(vertex_oval)
+        vertex_oval = canvas.create_oval(vertex.x - VERTEX_SIZE / 2, vertex.y - VERTEX_SIZE / 2,
+                                         vertex.x + VERTEX_SIZE / 2, vertex.y + VERTEX_SIZE / 2,
+                                         width=2, fill=color, activeoutline="green", activewidth=3
+                                         )
+        canvas.tag_raise(vertex_oval)
         return vertex_oval
     else:
-        _canvas.coords(index,
-                       _vertex.x - VERTEX_SIZE / 2, _vertex.y - VERTEX_SIZE / 2,
-                       _vertex.x + VERTEX_SIZE / 2, _vertex.y + VERTEX_SIZE / 2
-                       )
-        _canvas.itemconfig(index, fill=_color)
-        _canvas.tag_raise(index)
+        canvas.coords(index,
+                      vertex.x - VERTEX_SIZE / 2, vertex.y - VERTEX_SIZE / 2,
+                      vertex.x + VERTEX_SIZE / 2, vertex.y + VERTEX_SIZE / 2
+                      )
+        canvas.itemconfig(index, fill=color)
+        canvas.tag_raise(index)
 
 
 def draw_edge(canvas, edge, index=None):
@@ -118,6 +137,10 @@ def draw_edge(canvas, edge, index=None):
     Draws edge on canvas or modifies a drawn one
 
     Returns: element index if it was created
+    Parameters:
+        canvas - tk.Canvas object
+        edge - instance of Edge class
+        index - int, ID of edge element on canvas (if None, it is created)
     """
     vertex1 = edge.vertices[0]
     vertex2 = edge.vertices[1]
@@ -138,6 +161,13 @@ def draw_edge(canvas, edge, index=None):
 def add_vertex_from_click(x, y, canvas, graph):
     """
     Adds new vertex to graph after mouse click on canvas.
+
+    Returns (ID of vertex element, new Vertex object)
+    Parameters:
+         x - horizonatal position on canvas (mouse position)
+         y - vertical position on canvas (mouse pos)
+         canvas - tkinter.Canvas object
+         graph - instance of Graph class
     """
     vertex = Vertex("", x=x, y=y)
     graph = graph + vertex
@@ -147,6 +177,15 @@ def add_vertex_from_click(x, y, canvas, graph):
 
 
 def move_focused(x, y, element_index, elements):
+    """
+    Changes vertex coordinates
+
+    Parameters:
+        x - new horizontal position of vertex on canvas
+        y - new vertical position of vertex on canvas
+        element_index - ID of element (vertex) on canvas
+        elements - dictionary of elements on canvas
+    """
     if element_index is None or element_index not in elements.keys():
         return None
     element = elements[element_index]
@@ -157,6 +196,16 @@ def move_focused(x, y, element_index, elements):
 
 
 def add_line(vertices, canvas, graph):
+    """
+    Adds connection between vertices after dragging by left button on mouse
+
+    Creates Edge object, saves it to Graph and draws it
+    Returns (ID of new edge, new Edge object)
+    Parameters:
+        vertices - tuple of Vertex class instances to be connected
+        canvas - tkinter.Canvas object
+        graph - instance of Graph class
+    """
     vertex1, vertex2 = vertices
     if vertex1 is not None and vertex2 is not None:
         edge = Edge((vertex1, vertex2), graph=graph)
@@ -170,7 +219,14 @@ def add_line(vertices, canvas, graph):
 
 def remove_object(object_index, elements, graph, canvas):
     """
-    Removes component from graph object and from canvas
+    Removes component from Graph object and from canvas
+
+    Returns modified Graph object
+    Parameters:
+        object_index - ID of element on canvas to be removed
+        element - dictionary of all elements on canvas
+        graph - instance of Graph class
+        canvas - tkinter.Canvas object
     """
     if object_index:
         canvas.delete(object_index)
